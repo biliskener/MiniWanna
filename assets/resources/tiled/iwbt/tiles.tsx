8531ddfb4de699e71f0ab74e7e5e5c632c0b4262
biliskener <arcblade@163.com>
2021-10-27 19:08:38 +0800

    解决对象层的黑线问题

--- assets/resources/tiled/iwbt/tiles.tsx
+++ assets/resources/tiled/iwbt/tiles.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="tiles" tilewidth="32" tileheight="32" tilecount="24" columns="4">
- <image source="tiles.png" width="128" height="192"/>
+<tileset version="1.5" tiledversion="1.7.2" name="tiles" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="24" columns="4">
+ <image source="tiles.png" width="133" height="199"/>
  <tile id="0">
   <properties>
    <property name="customType" value="block"/>
